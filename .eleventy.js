c0decc1247bad4027ba8a635206d68c19519244e
Alexander May <alexander.may@hey.com>
2026-02-08 17:47:53 +0100

    favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy({ "src/favicons": "/" });
 
   eleventyConfig.addWatchTarget("_site/css/output.css");
 
